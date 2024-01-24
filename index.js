190a53590b5309b341b7ee078e9ae16521b0e3a7
GlobalIt820 <santyglobalit@gmail.com>
2024-01-24 17:23:36 +0530

    render check

--- index.js
+++ index.js
@@ -44,70 +44,8 @@ let pdfDocument = {
     type: "buffer", // "stream" || "buffer" || "" ("" defaults to pdf)
 };
 
-
 app.get('/', (req, res) => {
     res.render('home');
-});
-
-app.get('/generate_leaving_certificate', function (req, res) {
-    var html = fs.readFileSync(path.join(__dirname, "views/leaving_certificate.ejs"), "utf8");
-    let document = { ...pdfDocument };
-    document.html = html;
-    document.data = {
-        users: [
-            {
-                name: "Shyam",
-                age: "26",
-            },
-            {
-                name: "Navjot",
-                age: "26",
-            },
-            {
-                name: "Vitthal",
-                age: "26",
-            },
-        ]
-    }
-    pdf.create(document, options_portarit)
-        .then((pdfResp) => {
-            res.contentType("application/pdf");
-            res.send(pdfResp);
-        })
-        .catch((error) => {
-            res.send(error);
-        });
-});
-
-app.get('/generate_domicile_certificate', function (req, res) {
-    var html = fs.readFileSync(path.join(__dirname, "views/domicile.ejs"), "utf8");
-    let document = { ...pdfDocument };
-    document.html = html;
-    document.data = {
-        users: [
-            {
-                name: "Shyam",
-                age: "26",
-            },
-            {
-                name: "Navjot",
-                age: "26",
-            },
-            {
-                name: "Vitthal",
-                age: "26",
-            },
-        ]
-    }
-    console.log(document.data);
-    pdf.create(document, options_landscape)
-        .then((pdfResp) => {
-            res.contentType("application/pdf");
-            res.send(pdfResp);
-        })
-        .catch((error) => {
-            res.send(error);
-        });
 });
 
 async function fetchData(query) {
@@ -124,26 +62,63 @@ async function fetchData(query) {
     });
 }
 
-app.get('/generate_bonafide_certificate/:id', async function (req, res) {
+app.get('/generate_bonafide_certificate/:id', async function (req, res) 
+{
     var html = fs.readFileSync(path.join(__dirname, "views/bonafide.html"), "utf8");
-    let selectUser = `SELECT students.*,religions.religion_name AS religion,castes.caste_name AS caste FROM students INNER JOIN religions ON religions.id = students.religion INNER JOIN castes ON castes.id = students.caste WHERE (students.id = '${req.params.id}') limit 1`;
+    let selectUser = `SELECT bonafide_certificates.* FROM bonafide_certificates WHERE (bonafide_certificates.id = '${req.params.id}') limit 1`;
     let document = { ...pdfDocument };
     document.html = html;
     var result = await fetchData(selectUser)
-    console.log("studentData>>>>", result);
-
-    document.data = {
+    document.data = 
+    {
         "student" : result
     }
-
-    console.log(document.data);
-
     pdf.create(document, options_portarit).then((pdfResp) => {
         res.contentType("application/pdf");
         res.send(pdfResp);
     })
-        .catch((error) => {
-            res.send(error);
-        });
+    .catch((error) => {
+        res.send(error);
+    });
 });
 
+
+app.get('/generate_leaving_certificate/:id', async function (req, res) 
+{
+    var html = fs.readFileSync(path.join(__dirname, "views/leaving_certificate.html"), "utf8");
+    let selectUser = `SELECT bonafide_certificates.* FROM bonafide_certificates WHERE (bonafide_certificates.id = '${req.params.id}') limit 1`;
+    let document = { ...pdfDocument };
+    document.html = html;
+    var result = await fetchData(selectUser)
+    document.data = 
+    {
+        "student" : result
+    }
+    pdf.create(document,options_portarit).then((pdfResp) => {
+        res.contentType("application/pdf");
+        res.send(pdfResp);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+});
+
+app.get('/generate_domicile_certificate/:id', async function (req, res) 
+{
+    var html = fs.readFileSync(path.join(__dirname, "views/domicile.html"), "utf8");
+    let selectUser = `SELECT bonafide_certificates.* FROM bonafide_certificates WHERE (bonafide_certificates.id = '${req.params.id}') limit 1`;
+    let document = { ...pdfDocument };
+    document.html = html;
+    var result = await fetchData(selectUser)
+    document.data = 
+    {
+        "student" : result
+    }
+    pdf.create(document,options_portarit).then((pdfResp) => {
+        res.contentType("application/pdf");
+        res.send(pdfResp);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+});
