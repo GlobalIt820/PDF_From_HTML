78eb4fa42c1caa30bc540a32c6b20ba4577e061c
GlobalIt820 <santyglobalit@gmail.com>
2024-01-24 17:25:16 +0530

    sad

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var server = app.listen(3000, function () {
     console.log("Example app listening at http://localhost:3000")
 });
 
-const connection = require('./sql');
+// const connection = require('./sql');
 
 var options_portarit = {
     base: "http://localhost:3000",
@@ -51,27 +51,51 @@ app.get('/', (req, res) => {
 async function fetchData(query) {
     return new Promise((resolve, reject) => {
         console.log("query>>>",query)
-        connection.query(query, (error, results) => {
-            if (error) {
-                reject(error);
-            }
-            // Process the results here
-            resolve(results);
-            console.log("results>>>",results)
-        });
+        // connection.query(query, (error, results) => {
+        //     if (error) {
+        //         reject(error);
+        //     }
+        //     // Process the results here
+        //     resolve(results);
+        //     console.log("results>>>",results)
+        // });
     });
 }
 
-app.get('/generate_bonafide_certificate/:id', async function (req, res) 
+// app.get('/generate_bonafide_certificate/:id', async function (req, res) 
+// {
+//     var html = fs.readFileSync(path.join(__dirname, "views/bonafide.html"), "utf8");
+//     let selectUser = `SELECT bonafide_certificates.* FROM bonafide_certificates WHERE (bonafide_certificates.id = '${req.params.id}') limit 1`;
+//     let document = { ...pdfDocument };
+//     document.html = html;
+//     var result = await fetchData(selectUser)
+//     document.data = 
+//     {
+//         // "student" : result
+//         "name":"dharmik"
+//     }
+//     pdf.create(document, options_portarit).then((pdfResp) => {
+//         res.contentType("application/pdf");
+//         res.send(pdfResp);
+//     })
+//     .catch((error) => {
+//         res.send(error);
+//     });
+// });
+
+
+app.get('/generate_bonafide_certificate/', async function (req, res) 
 {
     var html = fs.readFileSync(path.join(__dirname, "views/bonafide.html"), "utf8");
-    let selectUser = `SELECT bonafide_certificates.* FROM bonafide_certificates WHERE (bonafide_certificates.id = '${req.params.id}') limit 1`;
     let document = { ...pdfDocument };
     document.html = html;
-    var result = await fetchData(selectUser)
+    // var result = await fetchData(selectUser)
     document.data = 
     {
-        "student" : result
+        // "student" : result
+        "student":[{
+            "name":"નામ"
+        }]
     }
     pdf.create(document, options_portarit).then((pdfResp) => {
         res.contentType("application/pdf");
